78c6b8808fcec3180ab7d87a683601c7d646810b
luiguip <luiguip@github.com>
2017-10-17 20:43:39 -0200

    simple tcp client

---
+++ tcp_client.py
@@ -0,0 +1,10 @@
+import socket
+
+target_host = "www.google.com"
+target_port = 80
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((target_host, target_port))
+client_socket.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+response = client_socket.recv(4096)
+print(response)
